03c3703f385cb39d5019d14ec1915e3acd8db5da
platypus <124347873+Nhannguyenus24@users.noreply.github.com>
2024-08-03 16:22:37 +0700

    Update private Route

--- src/App.jsx
+++ src/App.jsx
@@ -1,64 +1,40 @@
-// <<<<<<< 22127306_NguyễnTrọngNhân
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import SignUpPage from './pages/SignUpPage';
-import GuestPage from './pages/GuestPage';
-import ProfileSetting from './pages/SettingPage';
-import MainPage from './pages/MainPage';
-import LogInPage from './pages/LogInPage';
-import EventPage from './pages/EventPage';
-import CreateEvent from './pages/CreateEventPage';
-import './assets/App.css'
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import SignUpPage from "./pages/SignUpPage";
+import GuestPage from "./pages/GuestPage";
+import ProfileSetting from "./pages/SettingPage";
+import MainPage from "./pages/MainPage";
+import LogInPage from "./pages/LogInPage";
+import EventPage from "./pages/EventPage";
+import CreateEvent from "./pages/CreateEventPage";
+import "./assets/App.css";
+import { Navigate, Outlet } from "react-router-dom";
+
+const PrivateRoute = () => {
+  const isAuthenticated = !!localStorage.getItem("UserID"); // Kiểm tra xem userid có trong localStorage hay không
+
+  return isAuthenticated ? <Outlet /> : <Navigate to="/login" />;
+};
+
 function App() {
-
-  if (!localStorage.getItem("username")) {
-    localStorage.setItem("username", "#") // Set default value for UserID
-  }
-
   return (
     <Router>
       <Routes>
         <Route path="/" element={<GuestPage />} />
-        <Route path="/setting" element={<ProfileSetting />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/setting" element={<ProfileSetting />} />
+        </Route>
         <Route path="/signup" element={<SignUpPage />} />
         <Route path="/login" element={<LogInPage />} />
         <Route path="/mainpage" element={<MainPage />} />
-        <Route path="/mainpage/event/:id" element={<EventPage />} />
-        <Route path="/createvent" element={<CreateEvent />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/mainpage/event/:id" element={<EventPage />} />
+        </Route>
+        <Route element={<PrivateRoute />}>
+          <Route path="/createvent" element={<CreateEvent />} />
+        </Route>
       </Routes>
     </Router>
-  ); 
+  );
 }
 export default App;
-// =======
-// import { useState } from 'react'
-// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-// import SignUpForm from './SignUpForm';
-// import MainPage from './MainPage';
-// import LoginForm from './LoginForm';
-// import CreateEvent from './CreateEvent';
-// import AboutUs from './AboutUs';
-// import Profile from './Profile';
-// import ParticipantEventPage from './ParticipantEventPage';
-// import './App.css'
-
-// function App() {
-
-//   return (
-//     <>
-//       {/* <Router>
-//         <Routes>
-//           <Route path="/" element={<MainPage/>} /> 
-//           <Route path = "/profile" element = {<Profile/>} />
-//           <Route path="/aboutus" element={<AboutUs/>} />
-//           <Route path="/login" element={<LoginForm />} />
-//           <Route path="/signup" element={<SignUpForm />} />}
-//         </Routes>
-//       </Router> */}
-//       <ParticipantEventPage/>
-//     </>
-//   )
-// }
-
-// export default App
-// >>>>>>> 22127108_TrịnhQuốcHiệp
